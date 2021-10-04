6d71ae8c756bfe82aaf7b3e22f42b8e20fce40d9
alpalasaul <alpalasaul@gmail.com>
2021-10-03 23:16:21 -0500

    archivo task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+// importamos los datos del json
+import tasks from './sample/task.json';
+console.log(tasks); // observear los datos en la consola
 
-function App() {
-  return (
-    <div>Hello world</div>
-  );
+class App extends Component { // va en lugar de React.Component ya que lo importamos en la line 1
+
+  state = {
+    tasks: tasks // porpiedad: arreglo importado
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.tasks  }
+      </div>
+    );
+  }
 }
 
 export default App;
